cc1e98a2f740ebcf3cb4d9e11c64fe9bcce3bdd1
wiznaibus <wiznaibuswaltz@gmail.com>
2022-12-17 16:15:54 -0700

    update env

--- config/plugins.js
+++ config/plugins.js
@@ -1,1 +1,11 @@
-module.exports = ({ env }) => ({});
+module.exports = ({ env }) => ({
+    "provider": "aws-s3",
+    "providerOptions": {
+      "accessKeyId": env('AWS_ACCESSKEYID'),
+      "secretAccessKey": env('AWS_SECRETACCESSKEY'),
+      "region": env('AWS_REGION'),
+      "params": {
+        "Bucket": env('AWS_BUCKET')
+      }
+    }
+  });
